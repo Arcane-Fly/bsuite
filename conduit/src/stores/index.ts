19bfdb3b7af9ee894d79e5dc40341fc7ba9f50de
GaryOcean428 <braden.lang77@gmail.com>
2026-03-03 22:18:51 +0800

    chore: mark all submodules as dirty (R80.3, braden, BSU, crm7)

---
+++ conduit/src/stores/index.ts
@@ -0,0 +1,14 @@
+// Conduit stores barrel export
+export * from './aiStore';
+export * from './analyticsStore';
+export * from './candidateStore';
+export * from './communicationStore';
+export * from './complianceStore';
+export * from './interviewStore';
+export * from './jobDistributionStore';
+export * from './jobStore';
+export * from './offerStore';
+export * from './onboardingStore';
+export * from './pipelineStore';
+export * from './settingsStore';
+export * from './talentPoolStore';
